091aa85e81bd9e377dd68e03a1ec9a6e3f6c1e97
Dhaval Anjaria <mail@dhavalanjaria.com>
2018-02-23 00:08:10 +0530

    Added exercise history UIs
    Added the UI for exercise history and a few other activities. These activities are blank and need to be populated.

---
+++ app/src/main/java/com/dhavalanjaria/dyerest/ExerciseHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.dhavalanjaria.dyerest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExerciseHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_history);
+    }
+}
