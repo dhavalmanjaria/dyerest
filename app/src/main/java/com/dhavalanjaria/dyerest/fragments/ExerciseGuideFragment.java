133eed803fa36f4311f10eb4089193daf964b23b
Dhaval Anjaria <mail@dhavalanjaria.com>
2018-02-23 03:19:49 +0530

    Added UIs for comments and exercise guides
    Added UIs for some of the features.

---
+++ app/src/main/java/com/dhavalanjaria/dyerest/fragments/ExerciseGuideFragment.java
@@ -0,0 +1,11 @@
+package com.dhavalanjaria.dyerest.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dhavalanjaria.dyerest.R;
+
+public class ExerciseGuideFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/dhavalanjaria/dyerest/fragments/NewCommentDialogFragment.java
@@ -0,0 +1,10 @@
+package com.dhavalanjaria.dyerest.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Dhaval Anjaria on 2/23/2018.
+ */
+
+public class NewCommentDialogFragment extends Fragment {
+}
